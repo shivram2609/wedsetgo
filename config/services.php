91843c37978c3bd19edf0d53d5a67103a3065796
root <jaishivramji@gmail.com>
2018-01-19 19:06:10 +0530

    completing fb login

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
 		'client_id'=>'8BjjqUkMrb6n5GWNBOJLSPPm3',
 		'client_secret'=>'TtrEJCa8dWrZHkLKCU6NLUpY8wc4OEsipEDRfgAxAKcLLYNwWf',
 		'redirect' => 'http://localhost:8000/twitterCallBack'
-    ]
+    ],
     'facebook' => [
 		'client_id'=>'498803233853354',
 		'client_secret'=>'1ca8732555d066fcfa7d269e15b70558',
