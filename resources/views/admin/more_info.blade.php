6b262e2866359a0a55949319732d3c63d44f8b8f
root <jaishivramji@gmail.com>
2018-01-25 17:16:44 +0530

    testing captcha

--- resources/views/admin/more_info.blade.php
+++ resources/views/admin/more_info.blade.php
@@ -5,6 +5,10 @@
 	{!! Form::open() !!}
 			{!! Form::textarea('information', null, ['class' => 'form-control']) !!}</br></br>
 			{!! Form::submit('Submit', ['class' => 'btn btn-submit']) !!}
+			<span class="input-group-btn">
+					<a href="/view_professional/{{$id}}" class="btn btn-submit add-btn clear-category"> Cancle</a>
+				</span>
 	{!! Form::close() !!}
 </div>
 @endsection
+				
