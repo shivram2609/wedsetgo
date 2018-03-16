61d1f2f1d0534caad76206a51d0f3ed18290c1f0
root <jaishivramji@gmail.com>
2018-03-16 17:44:39 +0530

    add the 404 page

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -43,7 +43,9 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $e)
     {
         if ($e instanceof ModelNotFoundException) {
-            $e = new NotFoundHttpException($e->getMessage(), $e);
+			//echo $e->getStatusCode();
+			//die;
+           // $e = new NotFoundHttpException($e->getMessage(), $e);
         }
 
         return parent::render($request, $e);
