6ac255337c58734c348fe80a0ad574c32b22d2c6
root <jaishivramji@gmail.com>
2018-03-19 13:20:58 +0530

    changes in the controller file for email title

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -75,8 +75,8 @@ abstract class Controller extends BaseController
 		try {
 			$flag = Mail::send($template,$data, function($message) use ($data)
 			{
-				$message->from($this->email_from, $this->email_title);
-				$message->subject($this->email_subject);
+				$message->from($this->email_from, 'WedSetGo');
+			    $message->subject($this->email_subject);
 				$message->to($data['email']);
 			});
 			if ( $flag ) {
