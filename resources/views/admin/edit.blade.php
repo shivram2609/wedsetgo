550b69a4cd7c02ac19001980ee9cb77353ffaa5a
shivram2609 <shivram2609>
2018-08-02 10:40:06 +0530

    committing changes for is_change is NULL.

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -15,7 +15,7 @@
 		<?php if ($users->is_change == '0') { ?>
 		{!! Form::label('password', 'Password', ['class' => 'control-label']) !!}
 		{!! Form::text('password', (isset($users->user_password)?$users->user_password:''), ['class' => 'form-control', 'readonly' => 'true']) !!}
-		<?php } else if(($users->is_change == 'NULL')) { ?>
+		<?php } else if(($users->is_change == Null)) { ?>
 		{!! Form::label('password', 'Password', ['class' => 'control-label']) !!}
 		{!! Form::password('password', null, ['class' => 'form-control', 'readonly' => 'true']) !!}
 		<?php }?>
